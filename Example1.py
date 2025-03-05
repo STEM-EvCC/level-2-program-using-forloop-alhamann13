7aa05900f905c3feaee5e5f09c907cb2679477c1
alhamann13 <alhamann@students.everettcc.edu>
2025-03-05 19:43:16 +0000

    Completed

--- Example1.py
+++ Example1.py
@@ -1,5 +1,5 @@
 # Uses the range function to print numbers from 1 to 5
 # The range function creates a list of numbers. You can also choose the increment.
 # range(begin value, end value, optional increment)
-for i in range(1, 6):
-    print(i)
+for countUp in range(1, 6):
+    print(countUp)
--- main.py
+++ main.py
@@ -5,4 +5,31 @@ missionSuccess = [True, False, True, True, True, True, False]
 
 numberMissions = 0
 successfulMissions = 0
-missionsPre2000 = []
+missionsPre2000 = 0
+
+numberMissions = len(missionNames)
+print(f"Total number of missions: {numberMissions}")
+
+successfulMissions = sum(missionSuccess)
+print(f"The total number of successful missions : {successfulMissions}")
+
+successRate = (successfulMissions / numberMissions) * 100
+print(f"Success rate: {successRate:.2f}%")
+
+print("Missions launched before 2000: ")
+for name, year in zip(missionNames, missionYears):
+    if year < 2000:
+        print(" - ", name)
+
+#for mission_info in zip(missionNames, missionYears):
+ #   print(mission_info, mission_info[0], mission_info[1])
+
+#print()
+
+#for name, year in zip(missionNames, missionYears):
+#    print(name, year)
+
+#print()
+
+#for i in range(0, len(missionNames)):
+#    print(i, missionNames[i], missionYears[i])
